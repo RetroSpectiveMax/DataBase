d36d62eaa6e583af0f56c8e63fe158cf43e9b76d
Max <max.minkovsky7@gmail.com>
2019-05-05 12:13:48 -0700

    first commit

---
+++ MaxJavaScript.js
@@ -0,0 +1,9 @@
+function PasswordCheck(){
+	document.getElementById("response").innerHTML = "Thank You For Submiting this Form";
+	document.getElementById("DB").innerHTML = ' <a href="MaxDataBase.html"><button>Go To Data Base</button></a>';	
+}
+function TransferUser(){
+	console.log(window.location.href);
+
+	// window.location.pathname = 'MaxDataBase.html';
+}
